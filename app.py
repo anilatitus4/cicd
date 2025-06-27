8b5d31f0886f5b460fe3dcbc98a6f624ac8214bd
anilatitus4 <anilatitus4@gmail.com>
2025-06-27 13:46:37 -0400

    Add updated files

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,9 +9,7 @@ def home():
     return "Hello from GCP CI/CD Pipeline!"
 
 
-import os
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
-    
+   
